e4cb7992243ba8e990821ba8da5399bdb724a234
Paulo Ricardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-06-19 13:36:44 -0300

    Tratando erros

--- src/main/java/com/devserocaco/course/services/exeptions/ResourceNotFounExeption.java
+++ src/main/java/com/devserocaco/course/services/exeptions/ResourceNotFounExeption.java
@@ -1,5 +1,14 @@
 package com.devserocaco.course.services.exeptions;
 
-public class ResourceNotFounExeption {
+public class ResourceNotFounExeption extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFounExeption(Object id) {
+		super("Resource not found. Id" + id);
+	}
 
 }
